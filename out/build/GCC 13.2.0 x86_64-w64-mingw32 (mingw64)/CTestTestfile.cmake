1fed3f5e7aa526f3e3ec54c390f1820c2626740a
Julia <ula-anovikova@yandex.ru>
2024-07-06 04:16:46 +0700

    (Обновлено) Замечу в данном commit повторяется информация из main. (Добавлено) CmakeList.txt

---
+++ out/build/GCC 13.2.0 x86_64-w64-mingw32 (mingw64)/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Programming/git_scum/educational-practice
+# Build directory: D:/Programming/git_scum/educational-practice/out/build/GCC 13.2.0 x86_64-w64-mingw32 (mingw64)
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
